2d2acb7128ae775518e642e0188565c922a9fb80
Jessica <JCOJOHNS2@GMAIL.COM>
2019-07-27 12:36:33 -0500

    linked files to each other

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,21 @@
 //take the inputs from the menus from front end
+
 //search through the options in the API and results options
+
+var queryURL = "http://makeup-api.herokuapp.com/api/v1/products.json";
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+}
+    //creating loop to append each gif and rating to DOM
+
+
 //dynamically display options on the front end
+
 //give the user an option to choose from the options
+
 //once a product is chosen show product information
+
 //give an option to find it in a store near them
