a8edb2c4fa7e02448a387cd4fb132adde9d67089
Jessica <JCOJOHNS2@GMAIL.COM>
2019-07-27 12:22:16 -0500

    adding basic file structure

---
+++ assets/javascript/app.js
@@ -0,0 +1,6 @@
+//take the inputs from the menus from front end
+//search through the options in the API and results options
+//dynamically display options on the front end
+//give the user an option to choose from the options
+//once a product is chosen show product information
+//give an option to find it in a store near them
