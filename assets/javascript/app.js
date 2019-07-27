28d5cc272d578dc494ee8b7457e12bd125b6b36f
VJNap <vjnapolitano1364@gmail.com>
2019-07-27 13:39:40 -0500

    VN - Created variables to populate the queryUrl with user search parameters.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,14 +2,22 @@
 
 //search through the options in the API and results options
 
-var queryURL = "http://makeup-api.herokuapp.com/api/v1/products.json";
-var productType = // linked to product type field
+var queryURL = "http://makeup-api.herokuapp.com/api/v1/products.json?" + "product_type=" + productType + "&product_category=" + productCategory + "&brand=" + productBrand + "&product_tags=" + productTag + "&price_greater_than=" + priceMin + "&price_less_than=" + priceMax + "&rating_greater_than=" + ratingMin + "&rating_less_than" + ratingMax;
+var productType = "blush";// linked to product type field - product_type
+var productCategory = "powder";// linked to product subtype field - product_category
+var productBrand = "rejuva minerals";// linked to brand field - brand
+var productTag = "Hypoallergenic";// linked to product tags field - product_tags
+var priceMin = 0;// linked to price minimum field - price_greater_than
+var priceMax = 500;// linked to price maximum field - price_less_than
+var ratingMin = 0;// linked to rating minimum field - rating_greater_than
+var ratingMax = 5;// linked to rating maximum field - rating_less_than
 
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function (response) {
-}
+    console.log(response);
+});
     //creating loop to append each gif and rating to DOM
 
 
