cce1f2abce2f4cc6ed8732793f2854c03eb4ae92
VJNap <vjnapolitano1364@gmail.com>
2019-07-27 19:48:50 -0500

    VN - html link order adjusted by Will, troubleshooting queryURL construction to produce a successful console log.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,15 +2,17 @@
 
 //search through the options in the API and results options
 
-var queryURL = "http://makeup-api.herokuapp.com/api/v1/products.json?" + "product_type=" + productType + "&product_category=" + productCategory + "&brand=" + productBrand + "&product_tags=" + productTag + "&price_greater_than=" + priceMin + "&price_less_than=" + priceMax + "&rating_greater_than=" + ratingMin + "&rating_less_than" + ratingMax;
-var productType = "";// linked to product type field - product_type
-var productCategory = "";// linked to product subtype field - product_category
-var productBrand = "";// linked to brand field - brand
-var productTag = "";// linked to product tags field - product_tags
-var priceMin = 0;// linked to price minimum field - price_greater_than
-var priceMax = 500;// linked to price maximum field - price_less_than
-var ratingMin = 0;// linked to rating minimum field - rating_greater_than
-var ratingMax = 5;// linked to rating maximum field - rating_less_than
+var queryURL = "http://makeup-api.herokuapp.com/api/v1/products.json?" + "product_type=" + productType + "&product_category=" + productCategory + "&brand=" + productBrand + "&product_tags=" + productTag;
+//  + "&price_greater_than=" + priceMin + "&price_less_than=" + priceMax + "&rating_greater_than=" + ratingMin + "&rating_less_than" + ratingMax
+// var queryURL = "http://makeup-api.herokuapp.com/api/v1/products.json?product_tags=Vegan&product_type=blush&brand=e.l.f.&product_category=powder&price_less_than=10"
+var productType = "blush";// linked to product type field - product_type
+var productCategory = "powder";// linked to product subtype field - product_category
+var productBrand = "pacifica";// linked to brand field - brand
+var productTag = "Vegan";// linked to product tags field - product_tags
+// var priceMin = "";// linked to price minimum field - price_greater_than
+// var priceMax = "";// linked to price maximum field - price_less_than
+// var ratingMin = "";// linked to rating minimum field - rating_greater_than
+// var ratingMax = "";// linked to rating maximum field - rating_less_than
 
 $.ajax({
     url: queryURL,
