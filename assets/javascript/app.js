1aa2aae31b798475d6cf46be98f79f7997ad784e
VJNap <vjnapolitano1364@gmail.com>
2019-07-30 15:38:59 -0500

    VN - Adjusted variable order to fix broken queryURL, added console logs for variables to promote ease in troubleshooting future issues.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,26 +2,34 @@
 
 //search through the options in the API and results options
 
-var queryURL = "http://makeup-api.herokuapp.com/api/v1/products.json?" + "product_type=" + productType + "&product_category=" + productCategory + "&brand=" + productBrand + "&product_tags=" + productTag;
-//  + "&price_greater_than=" + priceMin + "&price_less_than=" + priceMax + "&rating_greater_than=" + ratingMin + "&rating_less_than" + ratingMax
-// var queryURL = "http://makeup-api.herokuapp.com/api/v1/products.json?product_tags=Vegan&product_type=blush&brand=e.l.f.&product_category=powder&price_less_than=10"
+// test query - var queryURL = "http://makeup-api.herokuapp.com/api/v1/products.json?product_tags=Vegan&product_type=blush&brand=pacifica&product_category=powder"
 var productType = "blush";// linked to product type field - product_type
 var productCategory = "powder";// linked to product subtype field - product_category
 var productBrand = "pacifica";// linked to brand field - brand
 var productTag = "Vegan";// linked to product tags field - product_tags
-// var priceMin = "";// linked to price minimum field - price_greater_than
-// var priceMax = "";// linked to price maximum field - price_less_than
-// var ratingMin = "";// linked to rating minimum field - rating_greater_than
-// var ratingMax = "";// linked to rating maximum field - rating_less_than
+var priceMin = "0";// linked to price minimum field - price_greater_than
+var priceMax = "100";// linked to price maximum field - price_less_than
+var ratingMin = "1";// linked to rating minimum field - rating_greater_than
+var ratingMax = "5";// linked to rating maximum field - rating_less_than
+var queryURL = "http://makeup-api.herokuapp.com/api/v1/products.json?" + "product_type=" + productType + "&product_category=" + productCategory + "&brand=" + productBrand + "&product_tags=" + productTag + "&price_greater_than=" + priceMin + "&price_less_than=" + priceMax + "&rating_greater_than=" + ratingMin + "&rating_less_than" + ratingMax;
+
 
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function (response) {
+    console.log(productType);
+    console.log(productCategory);
+    console.log(productBrand);
+    console.log(productTag);
+    console.log(priceMin);
+    console.log(priceMax);
+    console.log(ratingMin);
+    console.log(ratingMax);
+    console.log(queryURL);
     console.log(response);
 });
-    //creating loop to append each gif and rating to DOM
-
+//creating loop to append each gif and rating to DOM
 
 //dynamically display options on the front end
 
