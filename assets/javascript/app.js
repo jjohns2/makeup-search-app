2dc4fdee385ee4a8cbded3d6849eb16e36f88f2b
Jessica <JCOJOHNS2@GMAIL.COM>
2019-07-30 18:40:44 -0500

    no prog on code committing to pull from master

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,20 +2,31 @@
 
 //search through the options in the API and results options
 
+//productTag function
+//between each selection there needs to be a comma
+//empty string if nothing is selected
+//no comma if there is only 1
+
+// function productTagClean() {
+//     if () {
+
+//     }
+
+//     if else () {
+
+//     }
+
+//     if else () {
+
+//     }
+// }
+
+
 var queryURL = "http://makeup-api.herokuapp.com/api/v1/products.json";
 
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function (response) {
-}
-    //creating loop to append each gif and rating to DOM
-
-
-//dynamically display options on the front end
-
-//give the user an option to choose from the options
-
-//once a product is chosen show product information
-
-//give an option to find it in a store near them
+    console.log(response);
+});
