ee2e0cb39303dceab1fe598f683545bd8da7ec59
Anand Chaudhary <anand92490@gmail.com>
2019-08-03 14:00:27 -0500

    changed the button color

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -68,7 +68,7 @@ $(".productCategoryMenuClass a").on("click", function pushToProductCategory() {
       console.log(response);
 
 
-      var resultsContainerSection = $("<section class = 'results-container'>")
+      var resultsContainerSection = $("<section class = 'results-container'>");
         for (var i = 0; i < response.length; i++ ) {
           var singleResultDiv = $("<div class='result-container'>");
           var AppendImage = $("<img class='result'>");
@@ -100,3 +100,10 @@ function ARRtoString (arr) {
   console.log(z);
   return z;
 }
+
+$(document).ready(function(){
+  $(".dropdown-toggle").css({ "color": "#212529",
+    "background-color": "#ffc107",
+    "border-color": "#ffc107"});
+    $(".multiselect-selected-text").text('Select Tags');
+});
