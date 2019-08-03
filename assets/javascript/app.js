6daddbc712fd61f6d26084ab34e6bba263b039be
Jessica <JCOJOHNS2@GMAIL.COM>
2019-08-03 12:59:39 -0500

    clicking on image will bring to product page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,4 @@
-//take the inputs from the menus from front end
-
-// //search through the options in the API and results options
-
+//Variables for all the Things - JJ
 var tempProductType = "";
 var tempProductCat = "";
 var tempBrandMenu = "";
@@ -10,90 +7,112 @@ var tempMinRating = 0;
 var tempMinPrice = 1;
 var tempMaxRating = 5;
 var tempProductTag = "";
-var tempPT =[];
+var tempPT = [];
 
-//var queryURL = "http://makeup-api.herokuapp.com/api/v1/products.json?product_type=" + tempProductType + "&product_category=" + tempProductCat + "&brand=" + tempBrandMenu + "&price_greater_than=" + tempMinPrice + "&price_less_than=" + tempMaxPrice +"&rating_greater_than=" + tempMinRating + "&rating_less_than=" + tempMaxRating;
-// var productType = "";// linked to product type field - product_type
-// var productCategory = "";// linked to product subtype field - product_category
-// var productBrand = "";// linked to brand field - brand
-// var productTag = "";// linked to product tags field - product_tags
-// var priceMin = 0;// linked to price minimum field - price_greater_than
-// var priceMax = 500;// linked to price maximum field - price_less_than
-// var ratingMin = 0;// linked to rating minimum field - rating_greater_than
-// var ratingMax = 5;// linked to rating maximum field - rating_less_than
-
+//taking Product Type Input and formatting to Query - JJ
+//Changing Button Text to What Is Selected - JJ
 $(".productTypeMenuClass a").on("click", function pushToProductType() {
-    var tempPT = $(this).text();
-    tempProductType = tempPT.toLowerCase();
-    console.log(tempProductType);
-    return tempProductType;
-  });
+  var tempPT = $(this).text();
+  tempProductType = tempPT.toLowerCase();
+  console.log(tempProductType);
+  document.getElementById("productTypeButton").textContent = tempPT;
+  return tempProductType;
+});
 
+//taking Product Category Input and formatting to Query - JJ
+//Changing Button Text to What Is Selected - JJ
 $(".productCategoryMenuClass a").on("click", function pushToProductCategory() {
-    var tempPC = $(this).text();
-    tempProductCat = tempPC.toLowerCase();
-    console.log(tempProductCat);
-    return tempProductCat;
-  });
-
-  $(".brandMenuClass a").on("click", function pushToBrandMenuClass() {
-    var tempBMC = $(this).text();
-    tempBrandMenu = tempBMC.toLowerCase();
-    console.log(tempBrandMenu);
-    return tempBrandMenu;
-  });
-
-  $('example-getting-started').on('change', function(e, params) {
-    alert(e.target.value); // OR
-    alert(this.value); // OR
-    alert(params.selected); 
+  var tempPC = $(this).text();
+  tempProductCat = tempPC.toLowerCase();
+  console.log(tempProductCat);
+  document.getElementById("productCategoryButton").textContent = tempPC;
+  return tempProductCat;
 });
 
-  $("#submitMakeup").on("click", function () {
-    $("#MakeupDiv").empty();
-    tempMinPrice = document.getElementById("minPrice").value;
-    tempMaxPrice = document.getElementById("maxPrice").value;
-    tempMinRating = document.getElementById("minRating").value;
-    tempMaxRating = document.getElementById("maxRating").value;
+//taking Brand Input and formatting to Query - JJ
+//Changing Button Text to What Is Selected - JJ
+$(".brandMenuClass a").on("click", function pushToBrandMenuClass() {
+  var tempBMC = $(this).text();
+  tempBrandMenu = tempBMC.toLowerCase();
+  console.log(tempBrandMenu);
+  var uptempBMC = tempBMC.charAt(0).toUpperCase() + tempBMC.slice(1)
+  document.getElementById("brandButton").textContent = uptempBMC;
+  return tempBrandMenu;
+});
 
-    console.log(tempMinPrice);
-    console.log(tempMaxPrice);
-    console.log(tempMinRating);
-    console.log(tempMaxRating);
-    console.log(queryURL);
+//input collected from multi-select drop down menu - JJ
+$('example-getting-started').on('change', function (e, params) {
+  alert(e.target.value); // OR
+  alert(this.value); // OR
+  alert(params.selected);
+});
 
-    var queryURL = "https://makeup-api.herokuapp.com/api/v1/products.json?product_type=" + tempProductType + "&product_category=" + tempProductCat + "&brand=" + tempBrandMenu + "&price_less_than=" + tempMaxPrice;
+//AJAX Call
+//Taking Input - JJ
+$("#submitMakeup").on("click", function () {
+  $("#MakeupDiv").empty();
+  tempMinPrice = document.getElementById("minPrice").value;
+  tempMaxPrice = document.getElementById("maxPrice").value;
+  tempMinRating = document.getElementById("minRating").value;
+  tempMaxRating = document.getElementById("maxRating").value;
 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
+  console.log(tempMinPrice);
+  console.log(tempMaxPrice);
+  console.log(tempMinRating);
+  console.log(tempMaxRating);
+  console.log(queryURL);
+
+  var queryURL = "https://makeup-api.herokuapp.com/api/v1/products.json?product_type=" + tempProductType + "&product_category=" + tempProductCat + "&brand=" + tempBrandMenu + "&price_less_than=" + tempMaxPrice;
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
   }).then(function (response) {
-      console.log(response);
+    console.log(response);
 
-        for (var i = 0; i < response.length; i++ ) {
-          var AppendImage = $("<img>");
-          AppendImage.attr("src", response[i].image_link);
-          AppendImage.addClass("clickHere");
-          AppendImage.addClass("urlImage");
-          console.log(AppendImage);
-          $("#MakeupDiv").prepend(AppendImage)
-        }
-
-        // $(document).on('click', '.clickHere', function () {
-        //   var bringTobuy = $("<a>");
-        // }
-
+    for (var i = 0; i < response.length; i++) {
+      var AppendImage = $("<img>");
+      AppendImage.attr("src", response[i].image_link);
+      AppendImage.attr("link", response[i].product_link);
+      AppendImage.addClass("clickHere");
+      AppendImage.addClass("urlImage");
+      console.log(AppendImage);
+      $("#MakeupDiv").prepend(AppendImage)
+    }
   });
-  });
-
-  $(document).ready(function() {
-    $('#getting-started').multiselect();
 });
 
-function ARRtoString (arr) {
+//function to allow the drop-down multiselect to work - JJ
+$(document).ready(function () {
+  $('#getting-started').multiselect();
+});
+
+$('#getting-started').multiselect({
+  onChange: function() {
+      console.log($('#getting-started').val());
+      var tags = $('#getting-started option:selected');
+      for (var i = 0; i > tags.length; i++){
+        if (tag.indexOf(tags) > -1) {
+          tempPT.push([$(tag).val()]);
+        }
+    ARRtoString(tempPT);
+    return tempPT;
+      }
+    }
+  });
+
+//when an image is clicked a new tab where you can buy the product appears - JJ
+$(document).on('click', '.clickHere', function () {
+  var state = $(this).attr("link");
+  window.open(state);
+});
+
+//for Product Tag when it works - JJ
+function ARRtoString(arr) {
   var x = arr.toString();
   var y = x.replace(/ /g, "+");
   var z = y.toLowerCase();
   console.log(z);
   return z;
 }
+
